d5490b6ea0f081dba185c934aaaabd62084cebd7
Ali Keramati <92851368+alikera@users.noreply.github.com>
2023-09-30 11:39:43 +0330

    Delete MIPS directory

--- MIPS/adder.v
+++
@@ -1,6 +0,0 @@
-`timescale 1ns/1ns
-
-module adder(input [31:0] num_1, input [31:0] num_2, output [31:0] out);
-	assign out = num_1 + num_2;
-endmodule
-
--- MIPS/sign_extend.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns/1ns
-
-module sign_extend(input [15:0] num_1, output [31:0] out);
-	wire [16:0] ones, zeroes;
-	assign ones = 16'b1111111111111111;
-	assign out = num_1[15] ? {ones, num_1} : {16'd0, num_1};
-endmodule
-
-
